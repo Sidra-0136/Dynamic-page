a1b6c71769407c6b5c2bb57b0b6d0f6e6c34b739
Sidra Tul Muntaha <muntaha.0136@gmail.com>
2026-02-07 13:14:14 +0500

    dynamic page

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('#pink').addEventListener ('click', function (){
+  document.body.style.backgroundColor ='#F6C1CC'}
+)
+
+document.querySelector('#blue').addEventListener ('click', function(){
+  document.body.style.backgroundColor ='#CFE8F6'
+})
+
+document.querySelector('#yellow').addEventListener ('click', function () {
+  document.body.style.backgroundColor ='#FFF1B6'
+})
+
+document.querySelector('#green').addEventListener ('click', function () {
+  document.body.style.backgroundColor ='#CDEEE1'
+})
+
+document.querySelector('#reset').addEventListener ('click', function () {
+  document.body.style.backgroundColor ='white'
+})
